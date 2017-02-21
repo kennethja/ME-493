594b48be031676b996c6d3433407d0ec18fc1c2c
KAllison <kennethja24@gmail.com>
2017-02-21 14:56:19 -0800

    Update HW2.cpp

--- HW2.cpp
+++ HW2.cpp
@@ -24,6 +24,7 @@ public:
 	int movement = 0;
 	int c = 0;
 	int r = 0;
+	int curr = 0;
 
 	void set_up_array();
 	void create_grid();
@@ -32,8 +33,14 @@ public:
 
 void Human::set_up_array() {  //creates a double pointer array. This will be the "grid".
 	grid_arr = new int*[columns]();
+	int count = 0;
 	for (int i = 0; i < columns; i++) {
 		grid_arr[i] = new int[rows]();
+	}
+	for (int j = 0; j < rows; j++) {
+		for (int i = 0; i < columns; i++) {
+			grid_arr[i][j] = count++;
+		}
 	}
 }
 
@@ -50,25 +57,43 @@ void Human::create_grid(){
 }
 
 void Human::move() {
+	cout << "Pawn = " << pawn << "\nGoal = " << goal << endl;
 	cout << "Instructions: " << "Use numbers 1, 2, 3, 4 for right, left, down, and up. Respectively." << endl;
-	while (pawn != goal){
+	while(pawn != goal){
 
-	cin >> movement;
+		cin >> movement;
+		cout << endl;
 	switch (movement) {
 	case 1:
-		pawn = grid_arr[c + 1][r];
-		cout << "Current Postion" << pawn;
+		if (c + 1 <= columns - 1) {
+			pawn = grid_arr[c++][r];
+			cout << grid_arr[c][r] << endl; 
+			cout << "Current Postion: " << pawn;
+		}
+		break;
 	case 2:
-		pawn = grid_arr[c - 1][r];
-		cout << "Current Postion" << pawn;
+		if (c - 1 >= 0) {
+			pawn = grid_arr[c--][r];
+			cout << grid_arr[c][r] << endl;
+			cout << "Current Postion: " << pawn;
+		}
+		break;
 	case 3:
-		pawn = grid_arr[c][r + 1];
-		cout << "Current Postion" << pawn;
+		if (r + 1 <= rows - 1) {
+			pawn = grid_arr[c][r++];
+			cout << grid_arr[c][r] << endl;
+			cout << "Current Postion: " << pawn;
+		}
+		break;
 	case 4:
-		pawn = grid_arr[c][r - 1];
-		cout << "Current Postion" << pawn;
+		if (r - 1 >= 0) {
+			pawn = grid_arr[c][r--];
+			cout << grid_arr[c][r] << endl;
+			cout << "Current Postion: " << pawn;
+		}
+		break;
 	}
-			if (pawn = goal) {
+			if (pawn == goal) {
 				cout << "You have reached the goal" << endl;
 				break;
 			}
@@ -78,6 +103,7 @@ void Human::move() {
 
 int main()
 {
+	srand(time(NULL));
 	Human Fidget;
 	Fidget.set_up_array();
 	Fidget.create_grid();
