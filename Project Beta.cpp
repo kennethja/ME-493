bcbbf4bc5e982ada3debcca275f16989b423364b
KAllison <kennethja24@gmail.com>
2017-03-07 15:44:22 -0800

    Update Project Beta.cpp

--- Project Beta.cpp
+++ Project Beta.cpp
@@ -1,5 +1,7 @@
-// Project Beta.cpp : Defines the entry point for the console application.
-//
+//Kenneth Allison
+//ME 493
+//Project Beta
+//Worked with Scott Fourer and Bryant Clouse
 
 #include "stdafx.h";
 #include "iostream";
@@ -17,23 +19,29 @@ using namespace std;
 class Grid {
 public:
 	int **grid_arr;
+	int **new_rep;
 	int pawn = 0;
 	int goal = 0;
 	int movement = 0;
 	int c = 0;
 	int r = 0;
-	int rand_column = rand() % 10;
-	int rand_row = rand() % 10;
+	int rand_column = 8;//rand() % 15;
+	int rand_row = 8;//rand() % 15;
 	int R;
-	int columns = 10;
-	int rows = 10;
+	int columns = 15;
+	int rows = 15;
 	int size = columns * rows;
+	int move_count;
+	int counter = 0;
+	int testGstate = 0;
 
 	void setgrid();
 	void creategrid();
 	int update(int,int);
 	int reward(int new_state);
 	void TestE(int,int);
+	void TestF(int);
+	void TestG();
 };
 
 void Grid::setgrid() {  //creates a double pointer array. This will be the "grid".
@@ -82,16 +90,18 @@ int Grid::update(int state, int dir) {
 				pawn = grid_arr[c][--r];
 		}
 	}
-		cout << c << " X " << r << endl;
+		//cout << "Current Location: " << c << " X " << r << "    Current State: " << state << endl;
+		move_count++;
 		return pawn;
 }
 
 int Grid::reward(int new_pawn) {
-	int R = -1; //sets the default reward to zero
+	int R = -1; //sets the default reward
 	if (new_pawn == goal) {
 		R = R + 100;
-		new_pawn = pawn = 0;
+		new_pawn = pawn = c = r = 0;
 		TestE(new_pawn,pawn); 
+		
 	}
 	return R;
 }
@@ -99,17 +109,140 @@ int Grid::reward(int new_pawn) {
 //ensures that the agent(new_pawn) location on the grid is reset to zero 
 //once the goal is reached in the reward function.
 void Grid::TestE(int new_pawn, int pawn) {
-	int test;
-	assert(new_pawn == pawn);
+	assert(new_pawn == pawn == c == r);
 }
+
+//ensures that the final number of moes the agent takes 
+//is within resonable bounds of the optimal number of steps
+void Grid::TestF(int j) {
+	int op = abs(rand_column - 0) + abs(rand_row - 0);
+	if (move_count <= op*1.5) {
+		counter += 1;
+	}
+	if (j = 499) {
+		assert(counter >= 0.75 * 30);
+	}
+}
+
+//TestG creates a second state represantation that could be subsituted for the original state representation
+void Grid::TestG() {
+	if (c < rand_column && r < rand_row) {
+		testGstate = 1;
+	}
+	else if (c == rand_column && r < rand_row) {
+		testGstate = 2;
+	}
+	else if (c > rand_column && r < rand_row) {
+		testGstate = 3;
+	}
+	else if (c < rand_column && r == rand_row) {
+		testGstate = 4;
+	}
+	else if (c == rand_column && r == rand_row) {
+		testGstate = 5;
+	}
+	else if (c > rand_column && r == rand_row) {
+		testGstate = 6;
+	}
+	else if (c < rand_column && r > rand_row) {
+		testGstate = 7;
+	}
+	else if (c == rand_column && r > rand_row) {
+		testGstate = 8;
+	}
+	else if (c > rand_column && r > rand_row) {
+		testGstate = 9;
+	}
+	else {
+		c = r = 0;
+	}
+
+	int decide;
+	if (testGstate == 1) {
+		decide = rand() % 2;
+		if (decide <= 1) {
+			pawn = grid_arr[++r][c];
+		}
+		else {
+			pawn = grid_arr[r][++c];
+		}
+		}
+	else if (testGstate == 2) {
+		decide = rand() % 2;
+		if (decide <= 1.5) {
+			pawn = grid_arr[++r][c];
+		}
+		else {
+			pawn = grid_arr[r][++c];
+		}
+	}
+	else if (testGstate == 3) {
+		decide = rand() % 2;
+		if (decide <= 1) {
+			pawn = grid_arr[++r][c];
+		}
+		else {
+			pawn = grid_arr[r][--c];
+		}
+	}
+	else if (testGstate == 4) {
+		decide = rand() % 2;
+		if (decide <= 1.5) {
+			pawn = grid_arr[r][++c];
+		}
+		else {
+			pawn = grid_arr[++r][c];
+		}
+	}
+	else if (testGstate == 5) {
+			pawn = grid_arr[r][c];
+	}
+	else if (testGstate == 6) {
+		decide = rand() % 2;
+		if (decide <= 1.5) {
+			pawn = grid_arr[r][--c];
+		}
+		else {
+			pawn = grid_arr[++r][c];
+		}
+	}
+	else if (testGstate == 7) {
+		decide = rand() % 2;
+		if (decide <= 1) {
+			pawn = grid_arr[--r][c];
+		}
+		else {
+			pawn = grid_arr[r][++c];
+		}
+	}
+	else if (testGstate == 8) {
+		decide = rand() % 2;
+		if (decide <= 1.5) {
+			pawn = grid_arr[--r][c];
+		}
+		else {
+			pawn = grid_arr[r][++c];
+		}
+	}
+	else if (testGstate == 9) {
+		decide = rand() % 2;
+		if (decide <= 1) {
+			pawn = grid_arr[--r][c];
+		}
+		else {
+			pawn = grid_arr[r][--c];
+		}
+	}
+}
+
 
 class Agent {
 public:
 	double **Q;
 	double alpha = 0.1;
 	double epsilon = 0.1;
-	double gamma = 0.9;
-	int size = 100;
+	double gamma = 0.4;
+	int size = 225;
 	int response = 0;
 	int state = 0;
 	int new_state = 0;
@@ -131,8 +264,6 @@ public:
 	int decide();
 	int setQ_max();
 	void TestD(int);
-	void TestF();
-	void TestG();
 };
 
 Agent::Agent(Grid*grid) {
@@ -161,15 +292,14 @@ void Agent::update_Q(int movement) {
 
 void Agent::action() {
 	int movement;
-
 	while (1) {
 		movement = decide();
 		new_state = world->update(state, movement);
 		new_Reward = world->reward(new_state);
 		update_Q(movement);
-
 		if (new_state == world->goal) {
-			cout << "The computer has successfully located the goal. Run away!\n";
+			//cout << "The computer has successfully located the goal. Run away!\n";
+			state = new_state = new_Reward = response = 0;
 			break;
 		}
 	}
@@ -217,20 +347,42 @@ void Agent::TestD(int movement) {
 		assert(Q[state][movement] <= 100);
 }
 
-void Agent::TestF() {
-
-}
-
 int main() {
 	srand(time(NULL));
-	
+
+	ofstream myfile;
+	myfile.clear();
+	myfile.open("LearningCurve.csv");
+	//myfile << "Stat_Run" << "\t" << "Episode" << "\t" << "Move_Number" << endl;
+
 	Grid Fidget;
 	Fidget.setgrid();
 	Fidget.creategrid();
 
 	Agent Luna(&Fidget);
-	Luna.setQ();
-	Luna.action();
-	
+	int t[500]={0};
+	for (int i = 0; i < 500; i++) {
+		t[i] = 0;
+	}
+
+	//statistical runs
+	for (int i = 0; i < 30; i++) {
+		Luna.setQ();
+		for (int j = 0; j < 500; j++) {
+			Luna.action();
+			t[j] += Fidget.move_count;
+			//cout << Fidget.move_count << endl;
+			//cout << i << "\t" << j << "\t" << Fidget.move_count << endl;
+			//Fidget.TestF(j);
+			Fidget.move_count = 0;
+		}
+		cout << "Statistical Run: " << i << endl;
+	}
+
+	for (int i = 0; i < 500; i++) {
+		myfile << t[i]/30 << endl;
+	}
+
+	myfile.close();
 	return 0;
 }
